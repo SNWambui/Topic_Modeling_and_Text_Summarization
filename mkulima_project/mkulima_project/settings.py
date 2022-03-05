10841764cc6b09a62626437fbe41fd4927ebcc6a
SNWambui <stevedaviesndegwa@gmail.com>
2022-03-05 00:20:38 +0000

    Added heroku requirements

--- mkulima_project/mkulima_project/settings.py
+++ mkulima_project/mkulima_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku #important for heroku hosting
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,3 +137,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
+#heroku hosting requirement
+django_heroku.settings(locals())
