29e5691da489826da5fa240a6dc711c8f77fab18
SNWambui <stevedaviesndegwa@gmail.com>
2022-03-05 01:36:43 +0000

    added dj_database_url

--- mkulima_project/mkulima_project/settings.py
+++ mkulima_project/mkulima_project/settings.py
@@ -14,7 +14,7 @@ import os
 
 #important for heroku hosting
 import django_heroku 
-#import dj_database_url
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
